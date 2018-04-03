b5af5dacc6779e0309888cdf83a2e35f052afb08
GeekVitalik <vit.chernyshev@gmail.com>
2018-04-03 13:13:17 +0300

    minor changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -49,7 +49,8 @@ class JsonResponse {
 
     private String visibility;
     Wind wind;
-    public double getWind(){
+
+    public double getWind() {
         return wind.speed;
     }
 
@@ -76,6 +77,7 @@ class JsonResponse {
         private int sunrise;
         private int sunset;
     }
+
     private int id;
     private String name;
     private int cod;
@@ -85,18 +87,6 @@ class JsonResponse {
     }
 }
 
-/* Пример респонса
-        {"coord":{"lon":-0.13,"lat":51.51},
-        "weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],
-        "base":"stations",
-        "main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},
-        "visibility":10000,
-        "wind":{"speed":4.1,"deg":80},
-        "clouds":{"all":90},
-        "dt":1485789600,
-        "sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},
-        "id":2643743,"name":"London","cod":200}
-         */
 public class HelloWorld {
     public static void main(String[] args) {
         String api_url = "http://api.openweathermap.org/data/2.5/weather?q=Minsk,by&appid=3dd9eb7d424e58b41dc86c4d60dc4273";
@@ -130,4 +120,16 @@ public class HelloWorld {
             System.out.println("IOException");
         }
     }
+    /* Пример респонса
+        {"coord":{"lon":-0.13,"lat":51.51},
+        "weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],
+        "base":"stations",
+        "main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},
+        "visibility":10000,
+        "wind":{"speed":4.1,"deg":80},
+        "clouds":{"all":90},
+        "dt":1485789600,
+        "sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},
+        "id":2643743,"name":"London","cod":200}
+         */
 }
